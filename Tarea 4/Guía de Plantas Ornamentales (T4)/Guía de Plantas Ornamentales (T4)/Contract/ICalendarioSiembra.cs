5d7487ded81b49e6ae99a24c8a76d81ed8cbb029
Jey <jeirygarcia391@gmail.com>
2025-07-27 01:07:13 -0400

    Estructura de folder realizada

---
+++ Tarea 4/Guía de Plantas Ornamentales (T4)/Guía de Plantas Ornamentales (T4)/Contract/ICalendarioSiembra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guía_de_Plantas_Ornamentales__T4_.Contract
+{
+    internal class ICalendarioSiembra
+    {
+    }
+}
