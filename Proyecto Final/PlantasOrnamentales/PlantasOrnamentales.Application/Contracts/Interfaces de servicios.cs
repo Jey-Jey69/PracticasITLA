d01d66c8f4338b4b5f5322a5a385b535d7df3f59
Jey <jeirygarcia391@gmail.com>
2025-08-13 16:23:52 -0400

    Creating of the class

---
+++ Proyecto Final/PlantasOrnamentales/PlantasOrnamentales.Application/Contracts/Interfaces de servicios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantasOrnamentales.Application.Contracts
+{
+    internal class Interfaces_de_servicios
+    {
+    }
+}
---
+++ Proyecto Final/PlantasOrnamentales/PlantasOrnamentales.Application/MappingProfiles/AutoMapper para mapear entre entidades y DTOs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantasOrnamentales.Application.MappingProfiles
+{
+    internal class AutoMapper_para_mapear_entre_entidades_y_DTOs
+    {
+    }
+}
