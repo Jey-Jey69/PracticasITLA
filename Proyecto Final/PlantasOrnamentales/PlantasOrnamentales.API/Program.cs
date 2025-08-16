4f58f181857b9885ce7493f36c4239c6170e4b93
Jey <jeirygarcia391@gmail.com>
2025-08-16 15:21:25 -0400

    Finished frontend

--- Proyecto Final/PlantasOrnamentales/PlantasOrnamentales.API/Program.cs
+++ Proyecto Final/PlantasOrnamentales/PlantasOrnamentales.API/Program.cs
@@ -26,7 +26,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-// app.UseAuthorization();
+
+app.UseDefaultFiles();   
+app.UseStaticFiles();     
+
 
 app.MapControllers();
+
 app.Run();
